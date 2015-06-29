7d710a0b9301f5f183dbf35d446b5218ade876c7
Christina <christina.patterson@buchanan-edwards.com>
2015-06-29 15:27:04 -0400

    first commit

---
+++ public/app/app.module.js
@@ -0,0 +1,29 @@
+(function () {
+
+    var module = angular.module('app', [
+        'ui.router',
+        'ui.bootstrap',
+        'app.search',
+        'app.help'
+    ]);
+
+    function config($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('app', {
+                abstract: true,
+                views: {
+                    'header@': {
+                        templateUrl: 'app/shared/header.html'
+                    },
+                    'footer@': {
+                        templateUrl: 'app/shared/footer.html'
+                    }
+                }
+            });
+
+        $urlRouterProvider.otherwise('/');
+    }
+
+    module.config(config);
+})();
